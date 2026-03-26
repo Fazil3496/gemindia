aabb12b5aa8780bcf7f82584b6bb218468ed7aa1
M.D FAZIL <fazzi7790@gmail.com>
2026-03-27 03:13:26 +0530

    Final fix: Definitive app definition before config

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
+# Force Update 1from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_bcrypt import Bcrypt
 from datetime import datetime
